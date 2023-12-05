5f3a752bb42c277b2ddff28b374340b3d34b908d
Pedro <sanchez.deped24@triana.salesianos.edu>
2023-12-05 13:26:16 +0100

    Creación Usuario

--- AtrapaUnMillon/src/clases/Preguntas.java
+++ AtrapaUnMillon/src/clases/Preguntas.java
@@ -2,4 +2,10 @@ package clases;
 
 public class Preguntas {
 
+	private String nombre;
+	private String apellidos;
+	private double dinero;
+	private String residencia;
+	private int edad;
+	private String motivo;
 }
